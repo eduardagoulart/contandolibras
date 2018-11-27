a83914561935b4b0e3e617e96767ed9ee2feb1bd
eduardagoulart <goularteduarda.a@gmail.com>
2018-11-26 22:17:37 -0200

    tests status code 200

---
+++ coala/core/tests/test_jogos.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+
+
+class HomeTest(TestCase):
+    def setUp(self):
+        self.resp = self.client.get('/jogos')
+
+    def test_get(self):
+        """GET / deve retornar o status code 200"""
+        self.assertEqual(200, self.resp.status_code)
+
+    def test_template(self):
+        """Testa a existência do template"""
+        self.assertTemplateUsed(self.resp, 'core/jogos.html')
